f41352099a905e0655e1e1e36942596bc0cb3c7f
Alf Henderson <alfhenderson@gmail.com>
2017-11-03 14:52:24 +0100

    Refactor parmless hashes (Breaking change)
    ## Changes
    
    - Refactored plugin to uses hashes for lookup when finding redirect
    - Fixed a bug where redirects with parameters would not work in some cases
    - Added parameterless hash to plugin
    - Added WP-CLI CMD to create Paramless hash
    
    ## Deploying
    When deployed the plugin should enabled/disabled and the new command should be run straight after

---
+++ src/Commands/ParamLessHasher.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Bonnier\WP\Redirect\Commands;
+
+use Bonnier\WP\Redirect\Http\BonnierRedirect;
+use WP_CLI;
+
+class ParamLessHasher
+{
+    const CMD_NAMESPACE = 'bonnier redirect fix';
+
+    public static function register() {
+        WP_CLI::add_command(static::CMD_NAMESPACE, __CLASS__);
+    }
+
+    /**
+     * wp bonnier redirect paramless_hash_add
+     */
+    public function paramless_hash_add()
+    {
+        global $wpdb;
+        $redirects = collect($wpdb->get_results(
+            "SELECT `id`, `from` FROM wp_bonnier_redirects"
+        ));
+        $redirects->each(function ($redirect) use ($wpdb) {
+            $updatedRedirect = $wpdb->query(
+                $wpdb->prepare(
+                    "UPDATE wp_bonnier_redirects SET `paramless_from_hash` = MD5(%s), `from` = %s, `from_hash` = MD5(%s) WHERE `id` = %d",
+                    [
+                        str_before($redirect->from, '?'),
+                        $trimmedFrom = BonnierRedirect::trimAddSlash($redirect->from),
+                        $trimmedFrom,
+                        $redirect->id
+                    ]
+                )
+            );
+
+            if ($updatedRedirect) {
+                WP_CLI::success(sprintf("Fixed redirect %d from: %s",
+                        $redirect->id, $this->fix_encoding($redirect->from))
+                );
+            }
+        });
+
+        WP_CLI::success("Done fixing redirects");
+    }
+}
