43ef61b13501ea7fa44ecec9501122624741f800
Alf Henderson <alfhenderson@gmail.com>
2018-12-06 10:32:12 +0100

    IVD-535/Feature: Added override locale to findRedirectFor

--- wp-bonnier-redirect.php
+++ wp-bonnier-redirect.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Bonnier Redirect
- * Version: 2.1.0
+ * Version: 2.2.0
  * Plugin URI: https://github.com/BenjaminMedia/wp-bonnier-redirect
  * Description: This plugin creates redirects with support for Polylang
  * Author: Bonnier - Nicklas Kevin Frank
