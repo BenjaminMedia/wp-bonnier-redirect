e7b153da85665f247b842e94f7955ad00abbc4d3
Magnus Flor <magnus@m-flor.dk>
2019-10-07 12:36:39 +0200

    WILL-910 - Add redirect type filter to listview Added filters for redirect type and locale. Wrote a small guide, available here: https://docs.google.com/document/d/1cK9LiJjQh9nDoFbBxaZLgWLWmI-u3SnqdxDfA-uaxsw

--- src/Views/redirectsTable.php
+++ src/Views/redirectsTable.php
@@ -26,6 +26,58 @@
             </div>
             <?php
         }
+        if (!empty($this->redirectLocales)) {
+            ?>
+            <div style="height: 50px;">
+                <h2 class="screen-reader-text">Filter redirects list</h2>
+                <ul class="subsubsub">
+                    <li>
+                        <a class="<?php echo $this->request->get('redirect_locale') ? '' : 'current'; ?>"
+                           href="<?php echo esc_url(add_query_arg(['redirect_locale' => null])); ?>">All</a>
+                        |
+                    </li>
+                    <?php
+                    foreach($this->redirectLocales as $redirectLocale) {
+                        $locale = $redirectLocale['locale'];
+                        $amount = $redirectLocale['amount'];
+                        ?>
+                        <li>
+                            <a class="<?php echo $this->request->get('redirect_locale') === $locale ? 'current' : ''; ?>"
+                               href="<?php echo esc_url(add_query_arg(['redirect_locale' => $locale])); ?>"><?php echo strtoupper($locale) . ' (' . $amount . ')' ?></a>
+                            |
+                        </li>
+                        <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+            <?php
+        }
+        if (!empty($this->redirectTypes)) {
+            ?>
+            <label for="redirect-filter">Filter redirects</label>
+            <select id="redirect-filter" onchange="if (this.value) window.location.href = this.value">
+                <option
+                    value="<?php echo esc_url(add_query_arg(['redirect_type' => null])); ?>"
+                    <?php echo $this->request->get('redirect_type') ? '' : 'selected'; ?>>
+                    All
+                </option>
+                <?php
+                foreach ($this->redirectTypes as $redirectType) {
+                    $type = $redirectType['type'];
+                    $amount = $redirectType['amount'];
+                    ?>
+                    <option
+                        value="<?php echo esc_url(add_query_arg(['redirect_type' => $type])); ?>"
+                        <?php echo $this->request->get('redirect_type') === $type ? 'selected' : ''; ?>>
+                        <?php echo ucwords(preg_replace('/-/', ' ', $type)) . ' (' . $amount . ')' ?>
+                    </option>
+                    <?php
+                }
+                ?>
+            </select>
+            <?php
+        }
         $this->displaySearch();
         $this->display();
         ?>
